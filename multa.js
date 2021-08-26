711afd6cc0472ce90fceea3f716c25aab5d62e06
Willian de Souza Santos Vilella <86808935+Wyll-Vilella@users.noreply.github.com>
2021-08-26 18:11:39 -0300

    Aprendendo JavaScript
    Aprendendo JavaScript

---
+++ multa.js
@@ -0,0 +1,5 @@
+var vel = 61
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+if (vel>60) {//Condição seimples!}
+    console.log(`Você Ultrapassou a velocidade permitida. MULTADO`)}
+console.log(`Dirija sempre usando cinto de segurança!`)
